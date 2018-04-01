eaf29491e2b31d9c13d9640b1864d0d4e3b53030
Sachin <shaldipur79@gmail.com>
2018-04-01 19:38:26 -0400

    Lesson 3, Ex 1

---
+++ Lesson 3/ThrowArrayException.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+
+public class ThrowArrayException
+{
+
+
+    public static  void main (String[] args){
+
+        String[] names = new String[10];
+
+        names[0] = "Mike";
+        names[1] = "Henry";
+        names[2] = "George";
+        names[3] = "John";
+        names[4] = "Joe";
+        names[5] = "Ben";
+        names[6] = "Dave";
+        names[7] = "Tim";
+        names[8] = "Brian";
+        names[9] = "Tom";
+
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an array index >> ");
+        String index = input.nextLine();
+
+        int x = Integer.parseInt(index);
+
+            try {
+                System.out.println("The name associated with the array index is " + names[x] + ".");
+            } catch (Exception e) {
+                System.out.println((e.toString()));
+            }
+            input.close();
+    }
+
+}
