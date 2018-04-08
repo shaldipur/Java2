ff1414730d6ac726362467abdaaed0646143b898
Sachin <shaldipur79@gmail.com>
2018-04-08 11:53:19 -0400

    Lesson 5, Ex.1

---
+++ Lesson 5/Name.java
@@ -0,0 +1,83 @@
+/* Class: Java 2 (CIS 263AA)
+ *  Author: Sachin Haldipur
+ *  Lesson 5
+ *  Exercise 1
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+
+public class Name extends JFrame implements ActionListener {
+
+    //JFrame setup
+
+    //String name;
+
+    JLabel instruction = new JLabel("Please click the button:");
+
+    Font font = new Font("Arial", Font.BOLD, 16);
+
+    JButton button = new JButton("Click here");
+
+    JLabel display = new JLabel();
+
+    final int WIDTH = 275;
+
+    final int HEIGHT = 225;
+
+    String name = "";
+
+    ArrayList<String> names = new ArrayList<String>();
+
+
+    public Name(){
+
+        super("Display Names");
+
+        names.add("Bob");
+        names.add("Sally");
+        names.add("Mike");
+        names.add("Alice");
+        names.add("Tom");
+
+        setSize(HEIGHT, WIDTH);
+
+        setLayout(new FlowLayout());
+
+        instruction.setFont(font);
+
+        display.setFont(font);
+
+        add(instruction);
+
+        add(button);
+
+        add(display);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button.addActionListener(this);
+
+    }
+
+
+    //event handling action
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+        String name = names.get(0);
+
+        display.setText(name);
+
+        names.remove(0);
+
+    }
+
+
+
+
+
+}
---
+++ Lesson 5/NameDemo.java
@@ -0,0 +1,17 @@
+/* Class: Java 2 (CIS 263AA)
+ *  Author: Sachin Haldipur
+ *  Lesson 5
+ *  Exercise 1
+ *  This is to execute the code in Name.java
+ */
+
+
+public class NameDemo {
+
+    public static void main(String[] args){
+
+        Name frame = new Name();
+        frame.setVisible(true);
+
+    }
+}
